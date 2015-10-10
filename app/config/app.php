a43cd0e9a7f2dd58d560c217e9d949d49f780c7d
Timothy Kimathi <tkimathi@cytonn.com>
2015-10-10 10:13:33 +0000

    app.php edited online with Bitbucket Commented way/generator

--- app/config/app.php
+++ app/config/app.php
@@ -121,7 +121,7 @@ return array(
         'Illuminate\Validation\ValidationServiceProvider',
         'Illuminate\View\ViewServiceProvider',
         'Illuminate\Workbench\WorkbenchServiceProvider',
-        'Way\Generators\GeneratorsServiceProvider',
+        //'Way\Generators\GeneratorsServiceProvider',
         'Laracasts\Commander\CommanderServiceProvider',
         'Laracasts\Validation\ValidationServiceProvider',
         'Laracasts\Flash\FlashServiceProvider'
